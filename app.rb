d27bfbf780114acdc36411f059dfe11163666c4d
Edward Phillips <edd0ddi0@gmail.com>
2020-03-31 14:59:29 +0100

    added second view

--- app.rb
+++ app.rb
@@ -12,8 +12,13 @@ get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/lets' do
+
+  erb(:naming)
 end
