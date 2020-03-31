4b2b9c737a899fbdc69fcd62d19f071dbf17f772
Faye Carter <fayecarter333@gmail.com>
2020-03-31 08:38:42 +0100

    View controller updated

--- app.rb
+++ app.rb
@@ -3,14 +3,11 @@ require 'sinatra'
 get '/' do
   "hello!"
 end
+
 get '/secret' do
-  "Harambe died for our sins :( "
+  "Secrets"
 end
-get '/weapons_for_sale' do
-  "Ak-47 - $400 \n P90 - $100 "
-end
+
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  erb(:index)
 end
